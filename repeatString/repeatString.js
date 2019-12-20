19bb38756e0429b892ed533cb77bc04c3a10d184
Paolo Martinez <paolomartinezucsd@gmail.com>
2019-12-19 16:03:17 -0800

    Completed repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, numberOfTimes) {
+    if(numberOfTimes < 0) return 'ERROR';
+    let str = '';
+    for(let i = 0; i < numberOfTimes; i++) {
+        str += word;
+    }
+    return str;
 }
 
 module.exports = repeatString
