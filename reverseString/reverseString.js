0be4f3c6304091783acd5d6f458c032a238f48a2
Paolo Martinez <paolomartinezucsd@gmail.com>
2019-12-19 16:12:16 -0800

    Completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = '';
+    for(let i = str.length - 1; i >= 0; i--) {
+        reversed += str.charAt(i);
+    }
+    return reversed;
 }
 
 module.exports = reverseString
