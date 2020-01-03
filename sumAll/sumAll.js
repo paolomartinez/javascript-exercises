0dfadc234ec04a98d5ea7d3d2023336de616030d
Paolo Martinez <paolomartinezucsd@gmail.com>
2020-01-02 16:18:16 -0800

    Completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if(!Number.isInteger(numOne) || !Number.isInteger(numTwo) || numOne < 0 || numTwo < 0) {
+        return 'ERROR';
+    } 
+    let sum = 0;
+    let start = Math.min(numOne, numTwo);
+    let end = Math.max(numOne, numTwo);
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 module.exports = sumAll
