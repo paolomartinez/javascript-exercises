22b7cd4804741e5cc379f6221b2884b78ff6053f
Paolo Martinez <paolomartinezucsd@gmail.com>
2020-01-02 16:05:23 -0800

    Finished removeFromArray using filter method

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
-}
-
+const removeFromArray = function (...args) {
+    // the very first item in our list of arguments is the array, we pull it out with args[0]
+    const array = args[0];
+    return array.filter(val => !args.includes(val));
+  };
 module.exports = removeFromArray
