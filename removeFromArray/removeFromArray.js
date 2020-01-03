cca1c296fc79fd68ad718a471397aef0022c3d1f
Paolo Martinez <paolomartinezucsd@gmail.com>
2020-01-02 16:19:30 -0800

    removed space

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -2,5 +2,6 @@ const removeFromArray = function (...args) {
     // the very first item in our list of arguments is the array, we pull it out with args[0]
     const array = args[0];
     return array.filter(val => !args.includes(val));
-  };
+};
+
 module.exports = removeFromArray
