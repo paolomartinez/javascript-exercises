36a45111eebffba74f695066b2d466bb1f7cd230
Paolo Martinez <paolomartinezucsd@gmail.com>
2020-01-03 16:10:36 -0800

    Completed leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Leap years are years divisible by 4
+    if(year % 4 == 0) {
+        // Years divisible by 100 must also be divisible by 400
+        if(year % 100 == 0 && year % 400 != 0) {
+            return false;
+        }
+       return true;
+    }
+    return false;
 }
-
 module.exports = leapYears
