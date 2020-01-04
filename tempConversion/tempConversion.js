a9a76e30639f321e7b523f0af1e2403198d56ac8
Paolo Martinez <paolomartinezucsd@gmail.com>
2020-01-03 16:18:05 -0800

    Completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  // Formula: ([°F] − 32) × ​5⁄9
+  let converted = (temp - 32) * (5/9);
 
+  // Round the converted temperature
+  let rounded = Math.round( converted * 10 ) / 10;
+  return rounded;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  // Formula: [°C] × ​9⁄5 + 32
+  let converted = temp * (9/5) + 32;
+  
+  let rounded = Math.round( converted * 10 ) / 10;
+  return rounded;
 }
 
 module.exports = {
